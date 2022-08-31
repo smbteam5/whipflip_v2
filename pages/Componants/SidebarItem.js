1da7a5c5ae9bcb2ad43c78f5ea04de0067e26de5
Vishnu-nas <vishnupp@newagesmb.com>
2022-08-31 11:17:16 +0530

    nnn

--- pages/Componants/SidebarItem.js
+++ pages/Componants/SidebarItem.js
@@ -22,7 +22,7 @@ export default function SidebarItem({item}){
     {
         return (
             <a href={item?.path || "#"} className="sidebar-item plain">
-                {item.title}
+                {item?.title}
             </a>
         )
     }
