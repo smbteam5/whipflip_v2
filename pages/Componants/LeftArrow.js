59b73269acb1a51bcbfa3d536a0ac15680aa1cde
Vishnu-nas <vishnupp@newagesmb.com>
2022-11-22 16:43:15 +0530

    new

---
+++ pages/Componants/LeftArrow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LeftArrow() {
+  return (
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 476.213 476.213" >
+<polygon points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5   57.427,253.107 476.213,253.107 "/>
+</svg>
+    </div>
+  )
+}
+
+export default LeftArrow
